7a4f12e10f1451c8ae4f48230a8af37228aa78ca
Ex333 <haldas.mateusz@gmail.com>
2026-08-18 20:27:06 +0200

    Print lab added and print lesson added

--- 01_print.py
+++ 01_print.py
@@ -1,1 +1,28 @@
-#
+#display single string
+print('FR')
+
+#display multiple strings
+print('FR', 'ES', 'USA', 'IT', 'PL')
+
+#use variables
+country_france = 'FR'
+country_spain = 'ES'
+country_usa = 'USA'
+country_italy = 'IT'
+country_poland = 'PL'
+
+print(country_france, country_spain, country_usa, country_italy, country_poland, sep='\n')
+
+#do calculations
+pi = 3.14
+r = 5
+print('Area of circle with radius', r, 'is', pi * r * r)
+area = pi * r * r
+print('Area of circle with radius', r, 'is', area)
+
+# separators
+print(1, 2 , 3, 4, 5, sep='-')
+print('backslash', 'is', 'used', 'to', 'escape', 'characters', sep='\\')
+print('newline', 'is', 'used', 'to', 'start', 'a', 'new', 'line', sep='\n')
+
+print(x, y, z, sep='-')  # This line will raise an error because x, y, z are not defined
---
+++ 01_print_lab.py
@@ -0,0 +1,15 @@
+#1. Display the following text: TVP1, TVP2, TVN, Polsat, BBC, HBO, MTV. Use a single print statement.
+#2. Display the above text using the ; character as a separator.
+#3. Look at the text below. One part of it is repeated like a chorus in a song... Using a single print statement, display the entire text, but try to shorten the statement. The "chorus" — the repeated part — should appear only once in the print statement. Do you have an idea how to do this?
+
+#4. I like computers but better is TVP1 but better is TVP2 but better is TVN but better is Polsat but better is BBC but better is HBO but better is MTV
+
+#5. Declare variables ProgramName with the value 'BBC', Item with the value 'News', and Time with the value '18:00'.
+#Note: In this task, do not use string concatenation (joining strings). Use only the print statement!
+
+#Display the following text (pay attention to the period at the end!):
+
+#I like watching News at 18:00 on BBC .
+
+#6. Change the text so that the period is directly after BBC. We still do not use concatenation, but only a single print statement.
+
