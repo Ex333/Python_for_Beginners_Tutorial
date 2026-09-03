68fe981a4a841a71dc80c78e9d4d517d2a384feb
Ex333 <haldas.mateusz@gmail.com>
2026-09-03 21:03:24 +0200

    commit strings V1 tutorial

---
+++ 02_strings_v1.py
@@ -0,0 +1,17 @@
+filename = "december_production_report.xlsx"
+
+is_excel = filename.endswith("xlsx")
+
+is_word = filename.endswith(".docx")
+
+print("The file", filename)
+print("is_excel:", is_excel)
+print(" - is word:", is_word)
+
+1 + 2
+"1" + "2"
+#"1" + 2
+"=" * 40
+
+print("10aaA".isalnum()) #there should me numbers or letters if yes it returns True
+print("aaa".isalpha()) # Check if there is a letter of yes it returns True
