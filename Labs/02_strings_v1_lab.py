62f002c2e5c9c792569addbba1b4e43be0075588
Ex333 <haldas.mateusz@gmail.com>
2026-09-07 20:21:19 +0200

    String lab V1 i have made

--- Labs/02_strings_v1_lab.py
+++ Labs/02_strings_v1_lab.py
@@ -30,3 +30,39 @@
 # Our string contains spaces and consists of multiple words,
 # so it is neither an alphabetic string nor an alphanumeric string.
 
+#1
+quote = "A good programmer is someone who always looks both ways before crossing a one-way street"
+
+#2
+print(quote.upper())
+
+#3
+print(quote.lower())
+
+#4
+print(quote.endswith("street"))
+
+#5
+print(quote.isupper())
+
+#6
+print(quote.upper().isupper())
+
+#7
+print(quote.find("one"))
+
+#8
+print(quote.replace("one", "1"))
+
+#9
+print(quote.replace("one", "1").replace("both", "2"))
+
+#10
+print(quote.split(sep=" "))
+
+# 11
+
+print(quote.isdigit())
+print(quote.isdecimal())
+print(quote.isalpha())
+print(quote.isalnum())
